8bce976a1da930da598d08bd0e384fc41823c008
QuyenLe2107 <quyenk55.mdc@gmail.com>
2023-06-08 11:31:45 +0700

    add

---
+++ Java_OPP/src/BT_Java_OPP_1/ThongTin.java
@@ -0,0 +1,13 @@
+package BT_Java_OPP_1;
+
+public class ThongTin {
+    public static void main(String[] args) {
+        NhanVien nv1 = new NhanVien("Lan",12);
+
+        int nv1Age = nv1.getAge();
+       // System.out.println(nv1Age);
+        System.out.println(nv1.getAge());
+    }
+
+
+}
